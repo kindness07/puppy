6e45ba9eeaa7841c457a0425752f09502fb6d644
Kindness <okeziekindness07@gmail.com>
2026-05-19 01:26:23 +0100

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kindness07/puppy
 
 go 1.26.3
+
+require github.com/kindness07/Dog v0.0.0-20260519001514-2ae8183660b7
--- puppy.go
+++ puppy.go
@@ -1,9 +1,18 @@
 package puppy
 
-func Bark() string{
+import "github.com/kindness07/Dog"
+
+func Bark() string {
 	return "woof!"
 }
 
 func Barks() string {
 	return "woof! woof! woof!"
+}
+func BigBark() string {
+	return Dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return Dog.WhenGrownUp(Barks())
 }
